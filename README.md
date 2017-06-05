34ae91a27675f8d1281c42a499e864c00d9e45cc
fanyun <cangliang@live.com>
2017-06-05 14:16:05 +0800

    v1.0.31
    添加日期查看,过去的日子就让他过去吧!

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 ![预览图](https://github.com/fateplayer/wxapp-cxylhl/blob/screenshot/screenshot.png)
 ### Thank to
 [Yiding He](http://runjs.cn/detail/ydp3it7b)
+[Yiding He的知乎](https://www.zhihu.com/people/he-yiding-23/)
 
 感谢支持!
-### LICENSE
-[MIT](https://github.com/fateplayer/wxapp-cxylhl/blob/master/LICENSE)
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,78 +1,33 @@
 //index.js
 //获取应用实例
 var util = require('../../utils/u1.js')
-var Lunar = require('../../utils/Lunar.js')
-var today = new Date();
-var iday = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+//var Lunar = require('../../utils/Lunar.js')
+
 var drinks = util.drinks
 var directions = util.directions
 var app = getApp()
 Page({
   data: {
+    xin:'☰'
   },
-  
+
   onLoad: function () {
-
-    var week = today.getUTCDay()
+    var today = new Date();
+    var iday = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+    var week = getweek(today.getUTCDay())
     var year = today.getFullYear()
-    var month = today.getMonth()+1
+    var month = today.getMonth() + 1
     var day = today.getDate()
-    var Hours =today.getHours()
-    var Minutes = today.getMinutes()
 
-    var stars=''
     this.setData({
-      date: '今天是'+year + '年' + month + '月' + day+ '日  ',
-      //未知BUG
-      lunar: ''//Lunar.getLunar()
-    })
-    switch (week) {
-      case 1:
-        this.setData({
-          week: "星期一"
-        })
-        break;
-      case 2:
-        this.setData({
-          week: "星期二"
-        })
-        break;
-      case 3:
-        this.setData({
-          week: "星期三"
-        })
-        break;
-      case 4:
-        this.setData({
-          week: "星期四"
-        })
-        break;
-      case 5:
-        this.setData({
-          week: "星期五"
-        })
-        break;
-      case 6:
-        this.setData({
-          week: "星期六"
-        })
-        break;
-      case 0:
-        this.setData({
-          week: "星期日"
-        })
-        break;
-    }
-    this.setData({
-      drinks: pickRandom(drinks,2),
+      date: '今天是' + year + '年' + month + '月' + day + '日  ' + week,
+      starttimes: year + '-' + month + '-' + day,
+      drinks: pickRandom(iday,drinks, 2),
       direction: directions[random(iday, 2) % directions.length],
       stars: star(random(iday, 6) % 5 + 1),
-      goodlists: pickTodaysLuck(1),
-      splitlists: pickTodaysLuck(0)
+      goodlists: pickTodaysLuck(today,iday,1),
+      splitlists: pickTodaysLuck(today,iday,0)
     })
-  },
-  dzinput_bindfocus:function(){
-    
   },
   onShareAppMessage: function () {
     return {
@@ -80,9 +35,52 @@ Page({
       desc: '程序员老黄历!',
       path: 'pages/index/index'
     }
+  },
+  datePickerBindchange: function (e) {
+    var today = new Date(e.detail.value)
+    var week = getweek(today.getUTCDay())
+    var year = today.getFullYear()
+    var month = today.getMonth() + 1
+    var day = today.getDate()
+    var iday = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+    this.setData({
+      date: year + '年' + month + '月' + day + '日  ' + week,
+      starttimes: year + '-' + month + '-' + day,
+      drinks: pickRandom(iday, drinks, 2),
+      direction: directions[random(iday, 2) % directions.length],
+      stars: star(random(iday, 6) % 5 + 1),
+      goodlists: pickTodaysLuck(today, iday, 1),
+      splitlists: pickTodaysLuck(today, iday, 0)
+    })
   }
 })
 
+function getweek(week) {
+  var iweek;
+  switch (week) {
+    case 1:
+      iweek = "星期一";
+      break;
+    case 2:
+      iweek = "星期二";
+      break;
+    case 3:
+      iweek = "星期三";
+      break;
+    case 4:
+      iweek = "星期四";
+      break;
+    case 5:
+      iweek = "星期五";
+      break;
+    case 6:
+      iweek = "星期六";
+      break;
+    case 0:
+      iweek = "星期日";
+  }
+  return iweek
+}
 
 function star(num) {
   var result = "";
@@ -109,15 +107,15 @@ var activities = util.activities
 var tools = util.tools
 var varNames = util.varNames
 
-// 生成今日运势
-function pickTodaysLuck(e) {
+// 生成运势
+function pickTodaysLuck(today,iday,e) {
   var goodlist = [];
   var splitlist = [];
-  var _activities = filter(activities);
+  var _activities = filter(today,activities);
 
   var numGood = random(iday, 98) % 3 + 2;
   var numBad = random(iday, 87) % 3 + 2;
-  var eventArr = pickRandomActivity(_activities, numGood + numBad);
+  var eventArr = pickRandomActivity(iday,_activities, numGood + numBad);
 
 
   for (var i = 0; i < numGood; i++) {
@@ -125,21 +123,21 @@ function pickTodaysLuck(e) {
   }
 
   for (var i = 0; i < numBad; i++) {
-    splitlist.push(eventArr[numGood+i]);
+    splitlist.push(eventArr[numGood + i]);
   }
-  if(e==1){
+  if (e == 1) {
     return goodlist
-  }else if(e==0){
+  } else if (e == 0) {
     return splitlist
   }
 }
 
 // 去掉一些不合今日的事件
-function filter(activities) {
+function filter(e,activities) {
   var result = [];
 
   // 周末的话，只留下 weekend = true 的事件
-  if (isWeekend()) {
+  if (isWeekend(e)) {
 
     for (var i = 0; i < activities.length; i++) {
       if (activities[i].weekend) {
@@ -153,23 +151,23 @@ function filter(activities) {
   return activities;
 }
 
-function isWeekend() {
-  return today.getUTCDay() == 0 || today.getUTCDay() == 6;
+function isWeekend(e) {
+  return e.getUTCDay() == 0 || e.getUTCDay() == 6;
 }
 
 // 从 activities 中随机挑选 size 个
-function pickRandomActivity(activities, size) {
-  var picked_events = pickRandom(activities, size);
+function pickRandomActivity(iday,activities, size) {
+  var picked_events = pickRandom(iday,activities, size);
 
   for (var i = 0; i < picked_events.length; i++) {
-    picked_events[i] = parse(picked_events[i]);
+    picked_events[i] = parse(iday,picked_events[i]);
   }
 
   return picked_events;
 }
 
 // 从数组中随机挑选 size 个
-function pickRandom(array, size) {
+function pickRandom(iday,array, size) {
   var result = [];
 
   for (var i = 0; i < array.length; i++) {
@@ -185,7 +183,7 @@ function pickRandom(array, size) {
 }
 
 // 解析占位符并替换成随机内容
-function parse(event) {
+function parse(iday,event) {
   var result = { name: event.name, good: event.good, bad: event.bad };  // clone
 
   if (result.name.indexOf('%v') != -1) {
